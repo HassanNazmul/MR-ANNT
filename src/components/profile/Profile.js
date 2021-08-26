564cbf957d64f2cd8bbff22883a5e526544cf0ac
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2021-08-26 14:18:54 +0100

    Update Header Social Link

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -4,6 +4,9 @@ import styles from './ProfileStyle.module.css'
 import {FaFacebookSquare, FaLinkedin, HiOutlineMail} from 'react-icons/all';
 import abir from '../../img/abir.jpg'
 import nur from '../../img/nur.jpg'
+import nahid from '../../img/nahid.jpg'
+import toru from '../../img/toru.jpg'
+
 
 function Profile() {
 
@@ -40,7 +43,7 @@ function Profile() {
                             <div className={styles.fullName}>Nur Mohammed</div>
                             <div className={styles.iconDiv}><a href="https://www.facebook.com/md.nurulhuda1"
                                                                className={styles.icon}
-                            ><FaFacebookSquare/></a><a href="#"
+                            ><FaFacebookSquare/></a><a href="/"
                                                        className={styles.icon}
                             ><FaLinkedin/></a><a href="mailto:nurul.huda420.nh6@gmail.com"
                                                  className={styles.icon}><HiOutlineMail/></a>
@@ -48,39 +51,39 @@ function Profile() {
                         </div>
                     </div>
 
-                    {/*/!* NAHID *!/*/}
-                    {/*<div className="col-lg-3 col-sm-6">*/}
-                    {/*    <div className={styles.cardOuter}>*/}
-                    {/*        <div className={styles.cardContainer}>*/}
-                    {/*            <div><img className={styles} src={nahid} alt=""/></div>*/}
-                    {/*            <div className={styles.title}>NAHID</div>*/}
-                    {/*        </div>*/}
-                    {/*        <div className={styles.fullName}>Nazmul Hassan</div>*/}
-                    {/*        <div className={styles.iconDiv}><a href="https://www.facebook.com/Nazmul.naaz96"*/}
-                    {/*                                           className={styles.icon}*/}
-                    {/*        ><FaFacebookSquare/></a><a href="https://www.linkedin.com/in/nhassan96/"*/}
-                    {/*                                   className={styles.icon}*/}
-                    {/*        ><FaLinkedin/></a><a href="mailto:nazmul.naaz96@gmail.com*/}
-                    {/*            className={styles.icon} "><HiOutlineMail/></a></div>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
+                    {/* NAHID */}
+                    <div className="col-lg-3 col-sm-6">
+                        <div className={styles.cardOuter}>
+                            <div className={styles.cardContainer}>
+                                <div><img className={styles} src={nahid} alt=""/></div>
+                                <div className={styles.title}>NAHID</div>
+                            </div>
+                            <div className={styles.fullName}>Nazmul Hassan</div>
+                            <div className={styles.iconDiv}><a href="https://www.facebook.com/Nazmul.naaz96"
+                                                               className={styles.icon}
+                            ><FaFacebookSquare/></a><a href="https://www.linkedin.com/in/nhassan96/"
+                                                       className={styles.icon}
+                            ><FaLinkedin/></a><a href="mailto:nazmul.naaz96@gmail.com"
+                                                 className={styles.icon}><HiOutlineMail/></a></div>
+                        </div>
+                    </div>
 
-                    {/*/!* TORU *!/*/}
-                    {/*<div className="col-lg-3 col-sm-6">*/}
-                    {/*    <div className={styles.cardOuter}>*/}
-                    {/*        <div className={styles.cardContainer}>*/}
-                    {/*            <div><img className={styles} src={toru} alt=""/></div>*/}
-                    {/*            <div className={styles.title}>TORU</div>*/}
-                    {/*        </div>*/}
-                    {/*        <div className={styles.fullName}>Jubaer Ahmed</div>*/}
-                    {/*        <div className={styles.iconDiv}><a href="https://www.facebook.com/toru.jat"*/}
-                    {/*                                           className={styles.icon}*/}
-                    {/*        ><FaFacebookSquare/></a><a href="https://www.linkedin.com/in/jubaer-ahmed-7684a8157"*/}
-                    {/*                                   className={styles.icon}*/}
-                    {/*        ><FaLinkedin/></a><a href="mailto:torujat@gmail.com"*/}
-                    {/*                             className={styles.icon}><HiOutlineMail/></a></div>*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
+                    {/* TORU */}
+                    <div className="col-lg-3 col-sm-6">
+                        <div className={styles.cardOuter}>
+                            <div className={styles.cardContainer}>
+                                <div><img className={styles} src={toru} alt=""/></div>
+                                <div className={styles.title}>TORU</div>
+                            </div>
+                            <div className={styles.fullName}>Jubaer Ahmed</div>
+                            <div className={styles.iconDiv}><a href="https://www.facebook.com/toru.jat"
+                                                               className={styles.icon}
+                            ><FaFacebookSquare/></a><a href="https://www.linkedin.com/in/jubaer-ahmed-7684a8157"
+                                                       className={styles.icon}
+                            ><FaLinkedin/></a><a href="mailto:torujat@gmail.com"
+                                                 className={styles.icon}><HiOutlineMail/></a></div>
+                        </div>
+                    </div>
 
 
                 </div>
