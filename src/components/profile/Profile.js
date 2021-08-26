acc8174384359a228805ca8a5f68ee7da3ae157c
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2021-08-26 13:10:10 +0100

    Fixed Bugs And Error

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -2,12 +2,8 @@ import React from 'react';
 import styles from './ProfileStyle.module.css'
 import {useHistory} from "react-router-dom";
 
-import {FaFacebookSquare, FaLinkedin, HiOutlineMail} from 'react-icons/all';
-
-import nahid from '../../img/nahid.jpg'
-import toru from '../../img/toru.jpg'
+import {FaFacebookSquare} from 'react-icons/all';
 import abir from '../../img/abir.jpg'
-import nur from '../../img/nur.jpg'
 
 function Profile() {
 
@@ -28,84 +24,97 @@ function Profile() {
                             </div>
                             <div className={styles.fullName}>Fayed Abir</div>
                             <div className={styles.iconDiv}>
-                                <a className={styles.icon}><FaFacebookSquare onClick={() => {
-                                    history.push(window.location = 'https://www.facebook.com/fayedabir')
-                                }}/></a>
-                                <a className={styles.icon}><FaLinkedin onClick={() => {
-                                    history.push(window.location = 'https://www.linkedin.com/in/fayed-abir-969916171')
-                                }}/></a>
-                                <a className={styles.icon}><HiOutlineMail onClick={() => {
-                                    history.push(window.location = 'mailto:fayedabirf4@gmail.com')
-                                }}/></a>
+                                <a href="https://www.facebook.com/fayedabir" className={styles.icon}><FaFacebookSquare/></a>
                             </div>
                         </div>
                     </div>
 
-                    {/* NUR */}
-                    <div className="col-lg-3 col-sm-6">
-                        <div className={styles.cardOuter}>
-                            <div className={styles.cardContainer}>
-                                <div><img className={styles} src={nur} alt=""/></div>
-                                <div className={styles.title}>NUR</div>
-                            </div>
-                            <div className={styles.fullName}>Nur Mohammed</div>
-                            <div className={styles.iconDiv}>
-                                <a className={styles.icon}><FaFacebookSquare onClick={() => {
-                                    history.push(window.location = 'https://www.facebook.com/md.nurulhuda1')
-                                }}/></a>
-                                <a className={styles.icon}><FaLinkedin onClick={() => {
-                                    history.push(window.location = '#')
-                                }}/></a>
-                                <a className={styles.icon}><HiOutlineMail onClick={() => {
-                                    history.push(window.location = 'mailto:nurul.huda420.nh6@gmail.com')
-                                }}/></a>
-                            </div>
-                        </div>
-                    </div>
 
-                    {/* NAHID */}
-                    <div className="col-lg-3 col-sm-6">
-                        <div className={styles.cardOuter}>
-                            <div className={styles.cardContainer}>
-                                <div><img className={styles} src={nahid} alt=""/></div>
-                                <div className={styles.title}>NAHID</div>
-                            </div>
-                            <div className={styles.fullName}>Nazmul Hassan</div>
-                            <div className={styles.iconDiv}><a className={styles.icon}
-                            ><FaFacebookSquare onClick={() => {
-                                history.push(window.location = 'https://www.facebook.com/Nazmul.naaz96')
-                            }}/></a><a
-                                className={styles.icon}
-                            ><FaLinkedin onClick={() => {
-                                history.push(window.location = 'https://www.linkedin.com/in/nhassan96/')
-                            }}/></a><a
-                                className={styles.icon}><HiOutlineMail onClick={() => {
-                                history.push(window.location = 'mailto:nazmul.naaz96@gmail.com')
-                            }}/></a></div>
-                        </div>
-                    </div>
+                    {/*    <div className={styles.cardOuter}>*/}
+                    {/*        <div className={styles.cardContainer}>*/}
+                    {/*            <div><img className={styles} src={abir} alt=""/></div>*/}
+                    {/*            <div className={styles.title}>ABIR</div>*/}
+                    {/*        </div>*/}
+                    {/*        <div className={styles.fullName}>Fayed Abir</div>*/}
+                    {/*        <div className={styles.iconDiv}>*/}
+                    {/*            <a className={styles.icon}><FaFacebookSquare onClick={() => {*/}
+                    {/*                history.push(window.location = 'https://www.facebook.com/fayedabir')*/}
+                    {/*            }}/></a>*/}
+                    {/*            <a className={styles.icon}><FaLinkedin onClick={() => {*/}
+                    {/*                history.push(window.location = 'https://www.linkedin.com/in/fayed-abir-969916171')*/}
+                    {/*            }}/></a>*/}
+                    {/*            <a className={styles.icon}><HiOutlineMail onClick={() => {*/}
+                    {/*                history.push(window.location = 'mailto:fayedabirf4@gmail.com')*/}
+                    {/*            }}/></a>*/}
+                    {/*        </div>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
 
-                    {/* TORU */}
-                    <div className="col-lg-3 col-sm-6">
-                        <div className={styles.cardOuter}>
-                            <div className={styles.cardContainer}>
-                                <div><img className={styles} src={toru} alt=""/></div>
-                                <div className={styles.title}>TORU</div>
-                            </div>
-                            <div className={styles.fullName}>Jubaer Ahmed</div>
-                            <div className={styles.iconDiv}><a className={styles.icon}
-                            ><FaFacebookSquare onClick={() => {
-                                history.push(window.location = 'https://www.facebook.com/toru.jat')
-                            }}/></a><a
-                                className={styles.icon}
-                            ><FaLinkedin onClick={() => {
-                                history.push(window.location = 'https://www.linkedin.com/in/jubaer-ahmed-7684a8157')
-                            }}/></a><a
-                                className={styles.icon}><HiOutlineMail onClick={() => {
-                                history.push(window.location = 'mailto:torujat@gmail.com')
-                            }}/></a></div>
-                        </div>
-                    </div>
+                    {/*/!* NUR *!/*/}
+                    {/*<div className="col-lg-3 col-sm-6">*/}
+                    {/*    <div className={styles.cardOuter}>*/}
+                    {/*        <div className={styles.cardContainer}>*/}
+                    {/*            <div><img className={styles} src={nur} alt=""/></div>*/}
+                    {/*            <div className={styles.title}>NUR</div>*/}
+                    {/*        </div>*/}
+                    {/*        <div className={styles.fullName}>Nur Mohammed</div>*/}
+                    {/*        <div className={styles.iconDiv}>*/}
+                    {/*            <a className={styles.icon}><FaFacebookSquare onClick={() => {*/}
+                    {/*                history.push(window.location = 'https://www.facebook.com/md.nurulhuda1')*/}
+                    {/*            }}/></a>*/}
+                    {/*            <a className={styles.icon}><FaLinkedin onClick={() => {*/}
+                    {/*                history.push(window.location = '#')*/}
+                    {/*            }}/></a>*/}
+                    {/*            <a className={styles.icon}><HiOutlineMail onClick={() => {*/}
+                    {/*                history.push(window.location = 'mailto:nurul.huda420.nh6@gmail.com')*/}
+                    {/*            }}/></a>*/}
+                    {/*        </div>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
+
+                    {/*/!* NAHID *!/*/}
+                    {/*<div className="col-lg-3 col-sm-6">*/}
+                    {/*    <div className={styles.cardOuter}>*/}
+                    {/*        <div className={styles.cardContainer}>*/}
+                    {/*            <div><img className={styles} src={nahid} alt=""/></div>*/}
+                    {/*            <div className={styles.title}>NAHID</div>*/}
+                    {/*        </div>*/}
+                    {/*        <div className={styles.fullName}>Nazmul Hassan</div>*/}
+                    {/*        <div className={styles.iconDiv}><a className={styles.icon}*/}
+                    {/*        ><FaFacebookSquare onClick={() => {*/}
+                    {/*            history.push(window.location = 'https://www.facebook.com/Nazmul.naaz96')*/}
+                    {/*        }}/></a><a*/}
+                    {/*            className={styles.icon}*/}
+                    {/*        ><FaLinkedin onClick={() => {*/}
+                    {/*            history.push(window.location = 'https://www.linkedin.com/in/nhassan96/')*/}
+                    {/*        }}/></a><a*/}
+                    {/*            className={styles.icon}><HiOutlineMail onClick={() => {*/}
+                    {/*            history.push(window.location = 'mailto:nazmul.naaz96@gmail.com')*/}
+                    {/*        }}/></a></div>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
+
+                    {/*/!* TORU *!/*/}
+                    {/*<div className="col-lg-3 col-sm-6">*/}
+                    {/*    <div className={styles.cardOuter}>*/}
+                    {/*        <div className={styles.cardContainer}>*/}
+                    {/*            <div><img className={styles} src={toru} alt=""/></div>*/}
+                    {/*            <div className={styles.title}>TORU</div>*/}
+                    {/*        </div>*/}
+                    {/*        <div className={styles.fullName}>Jubaer Ahmed</div>*/}
+                    {/*        <div className={styles.iconDiv}><a className={styles.icon}*/}
+                    {/*        ><FaFacebookSquare onClick={() => {*/}
+                    {/*            history.push(window.location = 'https://www.facebook.com/toru.jat')*/}
+                    {/*        }}/></a><a*/}
+                    {/*            className={styles.icon}*/}
+                    {/*        ><FaLinkedin onClick={() => {*/}
+                    {/*            history.push(window.location = 'https://www.linkedin.com/in/jubaer-ahmed-7684a8157')*/}
+                    {/*        }}/></a><a*/}
+                    {/*            className={styles.icon}><HiOutlineMail onClick={() => {*/}
+                    {/*            history.push(window.location = 'mailto:torujat@gmail.com')*/}
+                    {/*        }}/></a></div>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
 
 
                 </div>
